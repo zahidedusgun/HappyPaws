6605afda56a74775cc0724feb06cd2f941f3a313
Hatice Deveci <104465906+hatice-dvc@users.noreply.github.com>
2023-12-25 21:40:06 +0300

    [Feat] Common Layer Created

---
+++ Core/HappyPaws.Domain/Common/ICreatedByEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyPaws.Domain.Common
+{
+    public interface ICreatedByEntity
+    {
+        public DateTimeOffset CreatedOn { get; set; }
+        string CreatedByUserId { get; set; }
+    }
+}
---
+++ Core/HappyPaws.Domain/Common/IModifiedByEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyPaws.Domain.Common
+{
+    public interface IModifiedByEntity
+    {
+        public DateTimeOffset? ModifiedOn { get; set; }
+        string? ModifiedByUserId { get; set; }
+    }
+}
