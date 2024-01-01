7abacadd1d5f177c6540db78995b26ed830de6c5
baharerol <baharerol.79@gmail.com>
2024-01-02 00:50:47 +0300

    [Feature] Create, Update and Remove Commands for HealthRecord added.

---
+++ Core/HappyPaws.Application/Features/Commands/HealthRecord/RemoveHealthRecord/RemoveHealthRecordCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HappyPaws.Persistence.Contexts;
+using MediatR;
+
+namespace HappyPaws.Application.Features.Commands.HealthRecord.RemoveHealthRecord
+{
+    public class RemoveHealthRecordCommandHandler:IRequestHandler<RemoveHealthRecordCommandRequest, RemoveHealthRecordCommandResponse> 
+    {
+        public async Task<RemoveHealthRecordCommandResponse> Handle(RemoveHealthRecordCommandRequest request, CancellationToken cancellationToken)
+        {
+            var removeHealthRecord =
+                ApplicationDbContext.HealthRecordList.FirstOrDefault(hr =>
+                    hr.Id == request.Id);
+            ApplicationDbContext.HealthRecordList.Remove(removeHealthRecord);
+            return new RemoveHealthRecordCommandResponse
+            {
+                IsSuccess = true
+            };
+        }
+    }
+}
---
+++ Core/HappyPaws.Application/Features/Commands/HealthRecord/UpdateHealthRecord/UpdateHealthRecordCommandRequest.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyPaws.Application.Features.Commands.HealthRecord.UpdateHealthRecord
+{
+    public class UpdateHealthRecordCommandRequest: IRequest<UpdateHealthRecordCommandResponse>
+    {
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public string VetNotes { get; set; }
+        public DateTime VetVisitDate { get; set; }
+    }
+}
