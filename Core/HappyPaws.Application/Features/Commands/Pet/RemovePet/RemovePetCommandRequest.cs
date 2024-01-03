7d9ccb724eefbe662becef6befc1c79b7eecf69c
ÖZGE DİNÇER <ozgedincer@posta.mu.edu.tr>
2024-01-04 00:10:38 +0300

    [Feat] RemovePet

---
+++ Core/HappyPaws.Application/Features/Commands/Pet/RemovePet/RemovePetCommandRequest.cs
@@ -0,0 +1,15 @@
+﻿using HappyPaws.Application.Features.Commands.Pet.RemovePet;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyPaws.Application.Features.Commands.Pet.RemovePet
+{
+    public class RemovePetCommandRequest : IRequest<RemovePetCommandResponse>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ Core/HappyPaws.Application/Features/Commands/Pet/RemovePet/RemovePetCommandResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyPaws.Application.Features.Commands.Pet.RemovePet
+{
+    public class RemovePetCommandResponse
+    {
+        public bool IsSuccess { get; set; }
+    }
+}
