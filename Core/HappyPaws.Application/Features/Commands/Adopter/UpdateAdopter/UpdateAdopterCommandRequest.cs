07ca325c34b0ddfcaf31145b4fb646ce91b05e65
Hatice Deveci <104465906+hatice-dvc@users.noreply.github.com>
2024-01-04 00:00:45 +0300

    [Feat] Update Adopter Commands added

---
+++ Core/HappyPaws.Application/Features/Commands/Adopter/UpdateAdopter/UpdateAdopterCommandRequest.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyPaws.Application.Features.Commands.Adopter.UpdateAdopter
+{
+    public class UpdateAdopterCommandRequest : IRequest<UpdateAdopterCommandResponse>
+    {
+        public string AdopterId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
