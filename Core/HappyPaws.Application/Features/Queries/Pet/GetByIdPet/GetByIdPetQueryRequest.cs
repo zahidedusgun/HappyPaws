83e0fe8558dfcb91f0151a1b36e0bc11661d291e
ÖZGE DİNÇER <ozgedincer@posta.mu.edu.tr>
2024-01-04 00:11:55 +0300

    [Feat] GetByIdPet

---
+++ Core/HappyPaws.Application/Features/Queries/Pet/GetByIdPet/GetByIdPetQueryRequest.cs
@@ -0,0 +1,15 @@
+﻿using HappyPaws.Application.Features.Queries.Pet.GetByIdPet;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyPaws.Application.Features.Queries.Pet.GetByIdPet
+{
+    public class GetByIdPetQueryRequest : IRequest<GetByIdPetQueryResponse>
+    {
+        public Guid Id { get; set; }
+    }
+}
