d2068e60d18fc6c6f51db2fd888ec933f2565bc7
ÖZGE DİNÇER <ozgedincer@posta.mu.edu.tr>
2024-01-04 00:11:33 +0300

    [Feat] GetAllPet

---
+++ Core/HappyPaws.Application/Features/Queries/Pet/GetAllPet/GetAllPetQueryRequest.cs
@@ -0,0 +1,15 @@
+﻿using HappyPaws.Application.Features.Queries.Pet.GetAllPet;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyPaws.Application.Features.Queries.Pet.GetAllPet
+{
+    public class GetAllPetQueryRequest : IRequest<List<GetAllPetQueryResponse>>
+    {
+
+    }
+}
---
+++ Core/HappyPaws.Application/Features/Queries/Pet/GetAllPet/GetAllPetQueryResponse.cs
@@ -0,0 +1,25 @@
+﻿using HappyPaws.Domain.Entities;
+using HappyPaws.Domain.Common;
+using HappyPaws.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyPaws.Application.Features.Queries.Pet.GetAllPet
+{
+    public class GetAllPetQueryResponse
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Breed { get; set; }
+        public short Age { get; set; }
+        public Gender Gender { get; set; }
+        public AdoptionStatus AdoptionStatus { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public Adopter Adopter { get; set; }
+        public Guid AdopterId { get; set; }
+        public ICollection<Adoption> Adoptions { get; set; }
+    }
+}
