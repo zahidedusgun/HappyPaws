614d3e1d263b60685436aed615fcda0a31962786
ÖZGE DİNÇER <ozgedincer@posta.mu.edu.tr>
2024-01-04 00:11:11 +0300

    [Feat] UpdatePet

---
+++ Core/HappyPaws.Application/Features/Commands/Pet/UpdatePet/UpdatePetCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using HappyPaws.Application.Features.Commands.Pet.UpdatePet;
+using HappyPaws.Persistence.Contexts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyPaws.Application.Features.Commands.Pet.UpdatePet
+{
+    public class UpdatePetCommandHandler : IRequestHandler<UpdatePetCommandRequest, UpdatePetCommandResponse>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UpdatePetCommandHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<UpdatePetCommandResponse> Handle(UpdatePetCommandRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var pet = await _context.Pets.FindAsync(Guid.Parse(request.Id));
+
+                if (pet == null)
+                {
+                    return new UpdatePetCommandResponse { IsSuccess = false };
+                }
+
+                pet.Name = request.Name;
+                pet.Type = request.Type;
+                pet.Age = request.Age;
+
+
+                await _context.SaveChangesAsync();
+
+                return new UpdatePetCommandResponse { IsSuccess = true };
+            }
+            catch (Exception)
+            {
+                return new UpdatePetCommandResponse { IsSuccess = false };
+            }
+        }
+    }
+}
---
+++ Core/HappyPaws.Application/Features/Commands/Pet/UpdatePet/UpdatePetCommandResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyPaws.Application.Features.Commands.Pet.UpdatePet
+{
+    public class UpdatePetCommandResponse
+    {
+        public bool IsSuccess { get; set; }
+    }
+}
