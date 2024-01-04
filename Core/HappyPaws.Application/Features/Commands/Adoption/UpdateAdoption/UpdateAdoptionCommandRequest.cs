bc2e3d3403a06e712f2b5588c172e72ce83aadc2
ÖZGE DİNÇER <ozgedincer@posta.mu.edu.tr>
2024-01-04 20:04:37 +0300

    [Feat] UpdateAdoption

---
+++ Core/HappyPaws.Application/Features/Commands/Adoption/UpdateAdoption/UpdateAdoptionCommandRequest.cs
@@ -0,0 +1,17 @@
+﻿using HappyPaws.Domain.Enums;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyPaws.Application.Features.Commands.Adoption.UpdateAdoption
+{
+    public class UpdateAdoptionCommandRequest : IRequest<UpdateAdoptionCommandResponse>
+    {
+        public string Id { get; set; }
+        public string AdoptionNotes { get; set; }
+        public AdoptionStatus AdoptionStatus { get; set; }
+    }
+}
---
+++ Core/HappyPaws.Application/Features/Commands/Adoption/UpdateAdoption/UpdateAdoptionCommandResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyPaws.Application.Features.Commands.Adoption.UpdateAdoption
+{
+    public class UpdateAdoptionCommandResponse
+    {
+        public bool IsSuccess { get; set; }
+    }
+}
