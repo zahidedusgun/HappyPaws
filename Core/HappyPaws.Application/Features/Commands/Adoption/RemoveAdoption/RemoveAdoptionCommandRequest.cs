f22028699d8d73cdea33132192fa6113e91126d5
ÖZGE DİNÇER <ozgedincer@posta.mu.edu.tr>
2024-01-04 20:04:12 +0300

    [Feat] RemoveAdoption

---
+++ Core/HappyPaws.Application/Features/Commands/Adoption/RemoveAdoption/RemoveAdoptionCommandRequest.cs
@@ -0,0 +1,15 @@
+﻿using HappyPaws.Application.Features.Commands.Adoption.RemoveAdoption;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyPaws.Application.Features.Commands.Adoption.RemoveAdoption
+{
+    public class RemoveAdoptionCommandRequest : IRequest<RemoveAdoptionCommandResponse>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ Core/HappyPaws.Application/Features/Commands/Adoption/RemoveAdoption/RemoveAdoptionCommandResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyPaws.Application.Features.Commands.Adoption.RemoveAdoption
+{
+    public class RemoveAdoptionCommandResponse
+    {
+        public bool IsSuccess { get; set; }
+
+    }
+}
