9ab5928fdd2ac9874b0baef55e2e60de9ea2332d
ÖZGE DİNÇER <ozgedincer@posta.mu.edu.tr>
2024-01-04 20:04:58 +0300

    [Feat] GetAllAdoption

---
+++ Core/HappyPaws.Application/Features/Queries/Adoption/GetAllAdoption/GetAllAdoptionQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using HappyPaws.Persistence.Contexts;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyPaws.Application.Features.Queries.Adoption.GetAllAdoption
+{
+    public class GetAllAdoptionQueryHandler : IRequestHandler<GetAllAdoptionQueryRequest, List<GetAllAdoptionQueryResponse>>
+    {
+        public async Task<List<GetAllAdoptionQueryResponse>> Handle(GetAllAdoptionQueryRequest request, CancellationToken cancellationToken)
+        {
+            return ApplicationDbContext.AdoptionList.Select(adoption => new GetAllAdoptionQueryResponse
+            {
+                AdoptionDate = adoption.AdoptionDate,
+                AdoptionNotes = adoption.AdoptionNotes,
+                AdoptionStatus = adoption.AdoptionStatus,
+
+            }).ToList();
+        }
+    }
+}
