caf52b8b17ad7ce8f1b3f960271de2da8c0c5cd3
Hatice Deveci <104465906+hatice-dvc@users.noreply.github.com>
2024-01-04 21:19:07 +0300

    [Feat] GetByIdAdopter Query created

---
+++ Core/HappyPaws.Application/Features/Queries/Adopter/GetByIdAdopter/GetByIdAdopterQueryRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyPaws.Application.Features.Queries.Adopter.GetByIdAdopter
+{
+    public class GetByIdAdopterQueryRequest : IRequest<GetByIdAdopterQueryResponse>
+    {
+        public Guid AdopterId { get; set; }
+    }
+}
